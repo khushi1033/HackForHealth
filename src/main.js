bd02848f6d7275447343d82a147a343189a1ddc3
Khushi <146786769+khushi1033@users.noreply.github.com>
2024-03-02 12:31:36 -0500

    start

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { Map } from './map';
-import './styles.css';
-
-document.addEventListener("DOMContentLoaded", function() {
-  let mapElement = document.getElementById('map');
-  Map.loadGoogleMapsApi().then(function(googleMaps) {
-    Map.createMap(googleMaps, mapElement);
-  });
-});
